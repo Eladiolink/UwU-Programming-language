feb568c53e57ffbe3b109205ac2681658ce5b51f
Eládio Leal <Eladiolink1@gmail.com>
2024-04-10 14:15:52 -0300

    Implementação de execução de arquivo por argumento

--- src/main/kotlin/compiler/Main.kt
+++ src/main/kotlin/compiler/Main.kt
@@ -1,10 +1,36 @@
 package compiler
 
+import java.io.File
+import kotlin.Result
+
 fun main(args: Array<String>) {
+    var resGetFile = getFile(args[0])
+    var program: String = ""
+
+    if (resGetFile.isSuccess) {
+        program = resGetFile.getOrThrow()
+        println("\nArquivo do Programa:")
+        println(program)
+    } else {
+        val excecao = resGetFile.exceptionOrNull()
+        println("Erro ao ler o programa:\n$excecao\n")
+    }
+
     val table: compiler.symbolTable.SymbolTable = mutableListOf()
+
     println("Tokens: ")
-    println(compiler.lexer.run("teste", table))
+    println(compiler.lexer.run("test", table))
     println("Table: ")
     println(table)
-    println(args[0])
+}
+
+fun getFile(path: String): Result<String> {
+    return runCatching {
+        val arquivo = File(path)
+
+        if (!arquivo.exists()) {
+            throw IllegalArgumentException("Arquivo não encontrado: $path")
+        }
+        arquivo.readText()
+    }
 }
