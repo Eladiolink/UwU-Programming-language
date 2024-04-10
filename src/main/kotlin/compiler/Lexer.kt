5c9e3821c0d6d86f4cc4ba1f4147f177541be6d3
Eládio Leal <Eladiolink1@gmail.com>
2024-04-09 23:52:47 -0300

    mudanças

--- src/main/kotlin/compiler/Lexer.kt
+++ src/main/kotlin/compiler/Lexer.kt
@@ -2,12 +2,20 @@ package compiler.lexer
 
 import compiler.symbol.*
 import compiler.token.*
+<<<<<<< HEAD
 import kotlin.io.println
 
 fun run(text: String): List<Token> {
 
+=======
+import compiler.symbolTable.*
+
+fun run(text: String, table: SymbolTable): List<Token> {
+>>>>>>> 9fe63f472e1308fdccd309eeacf54cc7ed26cc27
     val t0 = Token(TokenType.PALAVRAS, 0u, "0", "example/UwU.u", 1u)
+    table.add(EntrySymbol('h', ValueType.VALUE_CHAR))
     val t1 = Token(TokenType.PALAVRAS, 0u, "0", "example/UwU.u", 1u)
+<<<<<<< HEAD
 
     val element: Symbol? =
             createSybol(
@@ -19,5 +27,8 @@ fun run(text: String): List<Token> {
         println(element.type)
     }
 
+=======
+    table.add(EntrySymbol("Hello", ValueType.VALUE_STR))
+>>>>>>> 9fe63f472e1308fdccd309eeacf54cc7ed26cc27
     return listOf(t0, t1)
 }
