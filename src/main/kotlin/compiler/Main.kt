550af2ae39a62ffb44ae8bab4d9812ba109b10af
Eládio Leal <Eladiolink1@gmail.com>
2024-04-10 12:20:31 -0300

    Configuração da taks de criação do JAR

--- src/main/kotlin/compiler/Main.kt
+++ src/main/kotlin/compiler/Main.kt
@@ -1,10 +1,10 @@
-package compiler 
+package compiler
 
-
-fun main() {
+fun main(args: Array<String>) {
     val table: compiler.symbolTable.SymbolTable = mutableListOf()
     println("Tokens: ")
     println(compiler.lexer.run("teste", table))
     println("Table: ")
     println(table)
+    println(args[0])
 }
