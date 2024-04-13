63435c3257319c962d2080310fa9b39ffdb1308f
raulpy271 <raulrufino0@gmail.com>
2024-04-13 12:54:43 -0300

    Evita percorrer entrada diversas vezes

--- src/main/kotlin/compiler/Lexer.kt
+++ src/main/kotlin/compiler/Lexer.kt
@@ -6,41 +6,37 @@ import compiler.token.*
 fun run(text: String, table: SymbolTable): Result<List<Token>> {
     var tokens: MutableList<Token> = mutableListOf()
     var rem = text
-    var lineTotal = contarCaracteresEspecificos(text, '\n')
-    var lineCount: Int
+    var lineCount = 1
 
     while (rem != "") {
         val result = matchToken(rem)
         if (result.isSuccess) {
             val (type, tokenStr, res) = result.getOrThrow()
+            var linesToAdd = 0
+            var index: UInt? = null
             rem = res
-            lineCount = (lineTotal - contarCaracteresEspecificos(rem, '\n')) + 1
-
-            if (lineCount > lineTotal) break
-
-            // TODO: Trocar RESERVADA_CHAR pelo nome do token, trocar 0u pela posição na tabela de
 
             if (type in listOf(TokenType.IDENTIFICADORES, TokenType.PALAVRAS)) {
-                val lineTable = EntrySymbol(tokenStr, entryType(tokenStr))
+                val entryType = getEntryType(tokenStr)
+                val lineTable = EntrySymbol(tokenStr, entryType)
                 if (lineTable !in table) {
                     table.add(lineTable)
                 }
 
-                var index = table.indexOf(lineTable)
+                index = table.indexOf(lineTable).toUInt()
+                if (type == TokenType.PALAVRAS && entryType == ValueType.VALUE_STR) {
+                    linesToAdd = tokenStr.count({ c: Char -> c == '\n'})
+                }
 
-                tokens.add(
-                        Token(
-                                type,
-                                TokenName.RESERVADA_CHAR,
-                                index.toUInt(),
-                                tokenStr,
-                                lineCount.toUInt()
-                        )
-                )
-                continue
             }
+            if (type in listOf(TokenType.ESPACO, TokenType.COMENTARIO)) {
+                linesToAdd = tokenStr.count({ c: Char -> c == '\n'})
+            }
+            // TODO: Trocar RESERVADA_CHAR pelo nome do token
+            tokens.add(Token(type, TokenName.RESERVADA_CHAR, index, tokenStr, lineCount.toUInt()))
+            lineCount += linesToAdd
 
-            tokens.add(Token(type, TokenName.RESERVADA_CHAR, null, tokenStr, lineCount.toUInt()))
+
         } else {
             return Result.failure(result.exceptionOrNull() ?: Throwable())
         }
@@ -48,7 +44,7 @@ fun run(text: String, table: SymbolTable): Result<List<Token>> {
     return Result.success(tokens)
 }
 
-fun entryType(token: String): ValueType {
+fun getEntryType(token: String): ValueType {
     if (token.matches(Regex("[a-zA-Z]\\w+"))) return ValueType.VALUE_IDENTIFICADOR
 
     if (token.matches(Regex("-?(\\d+)(\\.)(\\d+)"))) return ValueType.VALUE_FLOAT
@@ -87,12 +83,3 @@ fun matchToken(text: String): Result<Triple<TokenType, String, String>> {
     return Result.failure(Throwable(msg))
 }
 
-fun contarCaracteresEspecificos(texto: String, caracterEspecifico: Char): Int {
-    var contador = 0
-    for (caracter in texto) {
-        if (caracter == caracterEspecifico) {
-            contador++
-        }
-    }
-    return contador
-}
